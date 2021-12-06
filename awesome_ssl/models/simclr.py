5cd0df9ae23949a1038709444aa4b1b7b2a669c1
Yunxing Liao <Lucy7298@gmail.com>
2021-12-06 12:00:08 -0500

    started implementing simclr

--- awesome_ssl/models/simclr.py
+++ awesome_ssl/models/simclr.py
@@ -10,7 +10,6 @@ from torchmetrics.functional import accuracy
 from omegaconf import DictConfig
 from hydra.utils import instantiate
 from torchvision import transforms as T
-import copy
 
 class TrainStage(Enum): 
     PRETRAIN = 0 
@@ -42,14 +41,13 @@ LINEAR_FIT_TRAIN_TRANFORM = T.Compose([
 LINEAR_FIT_VAL_TRANFORM = T.Normalize(mean=[0.485, 0.456, 0.406],
                                       std=[0.229, 0.224, 0.225])
 
-class BYOL(pl.LightningModule): 
+class SimCLR(pl.LightningModule): 
     def __init__(self,
                 encoder_params: model_utils.ModuleConfig, 
                 projector_params: model_utils.ModuleConfig, 
-                predictor_params: model_utils.ModuleConfig,
-                tau: float, 
                 accumulate_n_batch: int, 
                 optimizer_params: model_utils.ModuleConfig, 
+                temperature: float, 
                 transform_1 : Optional[DictConfig] = None, 
                 transform_2 : Optional[DictConfig] = None,
                 eval_interval: Optional[int] = -1, #linear evaluate after linear evaluate epochs
@@ -58,32 +56,17 @@ class BYOL(pl.LightningModule):
                 randominit_target: Optional[bool] = True): 
         super().__init__()
         self.save_hyperparameters()
-        self.online_encoder = torch.nn.Sequential(
+        self.encoder = torch.nn.Sequential(
             model_utils.build_module(encoder_params), 
             model_utils.build_module(projector_params)
         )
-        if randominit_target: 
-            print("randomly initializing target encoder...")
-            self.target_encoder = torch.nn.Sequential(
-                model_utils.build_module(encoder_params), 
-                model_utils.build_module(projector_params)
-            )
-        else: 
-            print("copying target encoder...")
-            self.target_encoder = copy.deepcopy(self.online_encoder)
         self.eval_interval = eval_interval
         if self.eval_interval > 0: 
             self.classifier = model_utils.build_module(linear_evaluate_config)
 
-        for target_param in self.target_encoder.parameters(): 
-            target_param.requires_grad = False 
-
-        self.prediction_head = model_utils.build_module(predictor_params)
-
         self.automatic_optimization = False
         self.optimizer_params = optimizer_params
         self.accumulate_n_batch = accumulate_n_batch
-        self.t = tau
 
         if transform_1 is not None: 
             print("instantiating transform 1")
@@ -102,37 +85,39 @@ class BYOL(pl.LightningModule):
             self.transform_2 = DEFAULT_TRAIN_AUG
     
         self.train_stage = TrainStage.PRETRAIN
+        self.temperature = temperature 
 
     def get_representation(self, x): 
-        return self.prediction_head(self.online_encoder(x))
-
-    def calculate_loss(self, online_prediction, target): 
-        prediction_norm = torch.nn.functional.normalize(online_prediction, dim=-1)
-        target_norm = torch.nn.functional.normalize(target, dim=-1)  
-        return torch.sum((prediction_norm - target_norm)**2, dim=-1)
+        return self.prediction_head(self.encoder(x))
 
     def on_train_epoch_start(self): 
         if self.eval_interval > 0: 
             if self.current_epoch % self.eval_interval == 0: 
                 print(f"epoch {self.current_epoch}: doing linear fit")
-                self.online_encoder = self.online_encoder.eval()
+                self.encoder = self.encoder.eval()
                 self.prediction_head = self.prediction_head.eval() 
                 self.train_stage = TrainStage.LINEAR_FIT
             else: 
                 print(f"epoch {self.current_epoch}: doing pretrain")
-                self.online_encoder = self.online_encoder.train()
+                self.encoder = self.encoder.train()
                 self.prediction_head = self.prediction_head.train()
                 self.train_stage = TrainStage.PRETRAIN
 
     def get_pretrain_loss(self, enc_1, enc_2, stage): 
-        with torch.no_grad(): 
-            target_1 = self.target_encoder(enc_1).detach()
-            target_2 = self.target_encoder(enc_2).detach()
-        on_pred_1 = self.prediction_head(self.online_encoder(enc_1))
-        on_pred_2 = self.prediction_head(self.online_encoder(enc_2))
-        loss_1 = self.calculate_loss(on_pred_1, target_2)
-        loss_2 = self.calculate_loss(on_pred_2, target_1)
-        loss = (loss_1 + loss_2).mean()
+        N, _ = enc_1.shape
+        on_pred_1 = self.encoder(enc_1) 
+        on_pred_1 = on_pred_1 / on_pred_1.norm(dim=1, keepdims=True)
+        on_pred_2 = self.encoder(enc_2)
+        on_pred_2 = on_pred_2 / on_pred_2.norm(dim=1, keepdims=True)
+        # Yo what even is this loss
+        all_encodings = torch.cat((on_pred_1, on_pred_2), axis=0)
+        # pairwise similarity 
+        similarities = torch.exp(all_encodings @ all_encodings.T / self.temperature)
+        # zero out diagonals 
+        similarities.fill_diagonal_(0)
+        nums = torch.cat(torch.diagonal(similarities, N), torch.diagonal(similarities, -N))
+        denoms = torch.sum(similarities, dim=1)
+        loss = (nums / denoms).mean()
         self.log(f"{stage}/pretrain_loss", loss)
         return loss
     
@@ -173,11 +158,6 @@ class BYOL(pl.LightningModule):
             opt_class.step()
             opt_class.zero_grad()
 
-        # update the target network with the online network
-        with torch.no_grad(): 
-            for target_p, online_p in zip(self.target_encoder.parameters(), \
-                                          self.online_encoder.parameters()): 
-                target_p.data = self.t * target_p.data + (1 - self.t) * online_p.data
 
     def validation_step(self, batch, batch_idx):
         X, y = batch
@@ -194,14 +174,12 @@ class BYOL(pl.LightningModule):
     def configure_optimizers(self): 
         # encoder optimizer
         model_params = [
-            {'params': self.online_encoder.parameters()}, 
-            {'params': self.prediction_head.parameters()
-        }]
+            {'params': self.encoder.parameters()}, 
+        ]
         optimizer_enc = model_utils.build_optimizer(self.optimizer_params, model_params)
         if self.eval_interval < 0: 
             return {"optimizer": optimizer_enc}
 
-        # hard-coding classifier optimizer for now
         else: 
             classifier_params = [{
                 "params": self.classifier.parameters()
