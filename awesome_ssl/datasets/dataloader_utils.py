ce2c9657bd58a002468fae06eb2c0778b54eac9d
Yunxing Liao <Lucy7298@gmail.com>
2022-03-15 23:02:35 -0400

    update dataloader utils

--- awesome_ssl/datasets/dataloader_utils.py
+++ awesome_ssl/datasets/dataloader_utils.py
@@ -1,61 +1,98 @@
+from sklearn.pipeline import Pipeline
 from torch.utils.data import DataLoader, Dataset
 from pytorch_lightning.core.datamodule import LightningDataModule
-from torch.cuda import device_count
 import torch
 from hydra.utils import instantiate
 import os
-# from ffcv.loader import Loader, OrderOption
-# from ffcv.fields.basics import IntDecoder
-# from ffcv.transforms import ToTensor, Squeeze, Convert
+from ffcv.loader import Loader, OrderOption
+from ffcv.fields.basics import IntDecoder
+from ffcv.transforms import ToTensor, Squeeze, Convert, ToTorchImage
+from ffcv.fields.decoders import RandomResizedCropRGBImageDecoder
+from ffcv.pipeline.pipeline_spec import PipelineSpec
+from ffcv.fields import json
+from awesome_ssl.models.model_utils import LINEAR_FIT_TRAIN_TRANFORM
+import torchvision
+import numpy as np
 
-# def make_loader(path, batch_size, num_workers, image_pipeline): 
-#     pipelines = {
-#         "image": image_pipeline + [Convert(torch.float32)], # should probably change according to config
-#         "label": [IntDecoder(), ToTensor(), Squeeze()]
-#     }
-#     distributed_flag = device_count() > 0
-#     print("device count: ", device_count(), "distributed: ", distributed_flag)
-#     return Loader(path,
-#                 batch_size=batch_size,
-#                 num_workers=num_workers,
-#                 order=OrderOption.RANDOM,
-#                 pipelines=pipelines, 
-#                 distributed=distributed_flag)
+def make_loader(path, batch_size, num_workers, transforms_dict, additional_fields): 
+    decoder = RandomResizedCropRGBImageDecoder((224, 224))
+    pre_transforms = [ToTensor(), 
+                      ToTorchImage(), 
+                      Convert(torch.float32), 
+                      torchvision.transforms.Normalize(0, 255)]
+    pipelines = {}
+    pipelines["image"] = PipelineSpec(source='image',
+                                           decoder=decoder,  
+                                           transforms=pre_transforms + LINEAR_FIT_TRAIN_TRANFORM.transforms)
+    # # make a pipeline for each transform in transforms_dict 
+    # # put global views first
+    for idx, (k, v) in enumerate(transforms_dict["global"].items()): 
+        pipelines[f"image_g{idx}"] = PipelineSpec(source='image',
+                                                  decoder=decoder,
+                                                  transforms=pre_transforms + v)
 
-def return_train_val_dataloaders(config, shuffle_train=True): 
+    for idx, (k, v) in enumerate(transforms_dict.get("local", {}).items()): 
+        pipelines[f"image_l{idx}"] = PipelineSpec(source="image",
+                                                  decoder=decoder,
+                                                  transforms=pre_transforms + v)
+
+    for fieldname, fieldtype in additional_fields.items(): 
+        if fieldtype == 'label': 
+            pipelines[fieldname] = [IntDecoder(), ToTensor(), Squeeze()]
+    
+    print("my pipelines", pipelines)
+
+    distributed_flag = torch.distributed.is_initialized()
+    print("is loader distributed:", distributed_flag )
+    return Loader(path,
+                batch_size=batch_size,
+                num_workers=num_workers,
+                order=OrderOption.RANDOM,
+                pipelines=pipelines, 
+                distributed=distributed_flag)
+
+def make_batch_processor(transforms, additional_fields): 
+    def process_batch(batch): 
+        global_crops = batch[:len(transforms["global"])]
+        local_crops = batch[len(global_crops): len(transforms.get("local", {}))]
+        additional_data = []
+        idx = len(global_crops) + len(local_crops)
+        for fieldname, fieldtype  in additional_fields.items(): 
+            data = batch[idx]
+            if fieldtype == 'json': 
+                data = json.unpack(data)
+            additional_data.append(data)
+        return global_crops, local_crops, *additional_data
+    return process_batch
+
+def return_train_val_dataloaders(config): 
     
     batch_size = config.batch_size
     if config.num_workers > 0: 
         num_workers = config.num_workers
     else: 
-        num_workers = max(4*device_count(), 1)
+        num_workers = 24
     print(num_workers)
     
     _, ext = os.path.splitext(config.train_dataset.root)
-    # if ext == '.beton': 
-    #     train_transforms = instantiate(config.train_dataset.pipelines)
-    #     test_transforms = instantiate(config.val_dataset.pipelines)
-    #     train_dataloader = make_loader(config.train_dataset.root, 
-    #                                    batch_size, 
-    #                                    num_workers, 
-    #                                    train_transforms.image)
-    #     val_dataloader = make_loader(config.val_dataset.root, 
-    #                                  batch_size, 
-    #                                  num_workers, 
-    #                                  test_transforms.image)
-    # else: 
-    train_dataset = instantiate(config.train_dataset)
-    val_dataset = instantiate(config.val_dataset)
-    train_dataloader = DataLoader(train_dataset, 
-                        batch_size=batch_size, 
-                        shuffle=shuffle_train, 
-                        drop_last=True, 
-                        num_workers=num_workers)
+    if ext == '.beton': 
+        transforms = instantiate(config.transforms)
+        train_dataloader = make_loader(config.train_dataset.root, 
+                                       batch_size, 
+                                       num_workers, 
+                                       transforms, 
+                                       config.train_dataset.additional_fields)
+        val_dataloader = make_loader(config.val_dataset.root, 
+                                     batch_size, 
+                                     num_workers, 
+                                     transforms, 
+                                     config.val_dataset.additional_fields)
+        train_batch_processor = make_batch_processor(transforms, 
+                                                     config.train_dataset.additional_fields)
+        val_batch_processor = make_batch_processor(transforms, 
+                                                   config.val_dataset.additional_fields)
+        
+    else: 
+        raise Exception("Unsupported dataloader - You can only use ffcv now")
 
-    val_dataloader =  DataLoader(val_dataset, 
-                        batch_size=batch_size, 
-                        shuffle=False, 
-                        drop_last=False, 
-                        num_workers = num_workers)
-
-    return train_dataloader, val_dataloader
+    return train_dataloader, val_dataloader, train_batch_processor, val_batch_processor
